43329fcd71d190b6eb1641b8525cd77f8ce1e312
Nate Kandler <natekandler@gmail.com>
2017-04-18 15:21:46 -0700

    updates gsub to use \2

--- regex.rb
+++ regex.rb
@@ -1,15 +1,15 @@
-phone_numbers = ["773-899-0088", "(773)899-5555", "619-555-5555"]
-
-phone_list = {}
-phone_numbers.each do |phone|
-  /(^\d{3})|(?<=\()([\d]{3})/.match(phone)
-  if phone_list[$&]
-    phone_list[$&] << phone
-  else
-    phone_list[$&] = [phone]
-  end
-end
-p phone_list
+#phone_numbers = ["773-899-0088", "(773)899-5555", "619-555-5555"]
+#
+#phone_list = {}
+#phone_numbers.each do |phone|
+#  /(^\d{3})|(?<=\()([\d]{3})/.match(phone)
+#  if phone_list[$&]
+#    phone_list[$&] << phone
+#  else
+#    phone_list[$&] = [phone]
+#  end
+#end
+#p phone_list
 
 #def capture_groups(phone)
 #  /(^[0-9][0-9][0-9])|(?<=\()([0-9]{3})/.match(phone)
@@ -25,6 +25,7 @@ p phone_list
 #capture_groups("(773)899-0055")
 #capture_groups("619-555-5555")
 
-#phone = "(773)899-0088"
-#p phone.gsub(/(\()(\d{3})(\))/, "#{$2}-")
-
+phone = "(773)899-0088"
+p phone.gsub(/(\()(\d{3})(\))/, '\2-')
+  p "globals"
+  p "$2 #{$2}"
